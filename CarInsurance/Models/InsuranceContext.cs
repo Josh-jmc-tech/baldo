4750e397eb9641f938149044e90a263e4f17c40b
Joshua Baldon <baldonvaljoshua97@gmail.com>
2025-08-25 22:53:29 -0400

    adding files

---
+++ CarInsurance/Models/InsuranceContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace CarInsurance.Models
+{
+    public class InsuranceContext : DbContext
+    {
+        public InsuranceContext() : base("InsuranceDbContext") { }
+
+        public DbSet<Insuree> Insurees { get; set; }
+    }
+}
---
+++ CarInsurance/Models/Insuree.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarInsurance.Models
+{
+    public class Insuree
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public string Email { get; set; }
+
+        public string CarMake { get; set; }
+
+        public string CarModel { get; set; }
+
+        public int CarYear { get; set; }
+
+        public bool HasDUI { get; set; }
+
+        public int SpeedingTickets { get; set; }
+
+        public bool FullCoverage { get; set; }
+    }
+}
